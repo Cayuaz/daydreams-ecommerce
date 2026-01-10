03f0c84c3e9edb88fb2ac02427e3f6ca18201df6
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-01-10 17:54:45 -0400

    docs: install and setup swagger with swagger json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import { globalErrors } from "./middlewares/errors.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" with {type: "json"}
 
 const app = express();
 app.use(express.json());
@@ -13,5 +15,6 @@ app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 app.use(globalErrors);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 export { app };
