c54134d265658bce0ed9c5ed55d8c80d91dace4a
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-01-16 12:50:42 -0400

    feat: create footer layout

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,7 +1,30 @@
+import { ArrowRight } from "lucide-react";
+
 export const Footer = () => {
   return (
-    <footer className="">
-      <h1>Footer</h1>
+    <footer className="bg-[#222224] text-white py-6 flex flex-col gap-4">
+      <div className="px-8 flex flex-col gap-6">
+        <div className="flex gap-2 border-2 border-white rounded-sm mx-auto items-center justify-between sm:w-3/6 px-4 py-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Newsletter"
+            className="w-full outline-none"
+          />
+          <ArrowRight />
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <img src="/youtube.svg" alt="Logo do Youtube" />
+          <img src="/instagram.svg" alt="Logo do Instagram" />
+          <img src="/tiktok.svg" alt="Logo do TikTok" />
+        </div>
+      </div>
+      <hr className="w-full" />
+      <p className="px-8 mt-2 text-sm/6 lg:text-base">
+        Copyright © 2025 Daydreams. Todos os direitos reservados. Desenvolvido
+        por Cayuã.
+      </p>
     </footer>
   );
 };
