807f2e719c2619bbdcc28d64ea0d6c6eb93b7f16
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-01-21 16:20:30 -0400

    style: update footer design and layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,21 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Header } from "./Components/header/Header";
 import { Footer } from "./Components/Footer";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 function App() {
   // const navigation = useNavigation();
+
+  useEffect(() => {
+    // O %c avisa que o texto a seguir terá estilo CSS
+    const msg = "%c DAYDREAMS \n %c Dreamt and Built by Cayuã";
+
+    const titleStyle = "color: #000; font-size: 20px; font-weight: bold; ";
+    const subtitleStyle =
+      "color: #555; font-size: 14px; font-family: monospace;";
+
+    console.log(msg, titleStyle, subtitleStyle);
+  }, []);
 
   return (
     <div className="min-h-screen grid grid-cols-1 grid-rows-[auto_1fr_auto] text-center items-center">
--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -2,29 +2,36 @@ import { ArrowRight } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-(--primary-color) text-white py-6 flex flex-col gap-4">
-      <div className="px-8 flex flex-col gap-6">
-        <div className="flex gap-2 border-2 border-white rounded-sm mx-auto items-center justify-between sm:w-3/6 px-4 py-2">
+    <footer className="bg-(--primary-color) text-white py-6 flex flex-col gap-6">
+      <div className="px-8 flex flex-col lg:grid lg:grid-cols-3 justify-center gap-6">
+        <div className="flex gap-4 border-2 border-white rounded-sm mx-auto items-center justify-between px-3 py-2 w-4/6 sm:w-3/6 lg:w-5/6">
           <input
             type="text"
             name=""
             id=""
             placeholder="Newsletter"
-            className="w-full outline-none"
+            className="w-full outline-none text-sm lg:text-base"
           />
           <ArrowRight />
         </div>
+        <div className="flex flex-col gap-2">
+          <span className="text-base">Contato</span>
+          <span className="text-sm">
+            contact@daydreams.com.br | 09:00 - 17:00
+          </span>
+        </div>
         <div className="flex items-center justify-center gap-4">
-          <img src="/youtube.svg" alt="Logo do Youtube" />
-          <img src="/instagram.svg" alt="Logo do Instagram" />
-          <img src="/tiktok.svg" alt="Logo do TikTok" />
+          <img src="/youtube.svg" alt="Logo do Youtube" className="size-7 " />
+          <img
+            src="/instagram.svg"
+            alt="Logo do Instagram"
+            className="size-6 "
+          />
+          <img src="/tiktok.svg" alt="Logo do TikTok" className="size-6 " />
         </div>
       </div>
       <hr className="w-full" />
-      <p className="px-8 mt-2 text-sm/6 lg:text-base">
-        Copyright © 2025 Daydreams. Todos os direitos reservados. Desenvolvido
-        por Cayuã.
-      </p>
+      <p className="px-8 mt-2 text-sm/6">Copyright © 2025 Daydreams</p>
     </footer>
   );
 };
