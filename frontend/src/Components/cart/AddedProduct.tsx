13e26d0127093f57c613a9d86f45f6c4d8788bad
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-01-24 16:04:37 -0400

    feat: implement framer-motion animations for cart components

--- frontend/src/Components/cart/AddedProduct.tsx
+++ frontend/src/Components/cart/AddedProduct.tsx
@@ -3,6 +3,7 @@ import { useAddedProductStore } from "@/stores/useAddedProductStore";
 import { Check, X } from "lucide-react";
 import { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 const AddedProduct = () => {
   const { addedProduct, setAddedProduct } = useAddedProductStore();
@@ -31,50 +32,66 @@ const AddedProduct = () => {
   };
 
   return (
-    /* Em dispositivos móveis o container vem de baixo pra cima e fica centralizado (top-1/2 e -translate-y-1/2) 
-     Em dispositivos com telas maiores o container fica no topo a direita e aparece gradualmente (sm:left-auto sm:right-4 sm:translate-x-0)
-     */
-    <div
-      className={`w-full max-w-sm z-50 
-          bg-[#1a1a1a] py-6 px-4 rounded-md border shadow border-white/10 text-white fixed transition-all duration-700 ease-in-out left-1/2 sm:left-auto sm:right-4 sm:translate-x-0 -translate-x-1/2 ${addedProduct ? "top-1/2 -translate-y-1/2 opacity-100 scale-100 sm:top-24 sm:translate-y-0" : "opacity-0 scale-95 top-[110%] sm:top-24"}`}
-    >
+    /* O AnimatePresence é essencial na renderização condicional, em vez de usar a renderização no conteúdo do componente,
+    se usa em todo container, e o AnimatePresence com exit garantem que o componente permaneça na tela mesmo que o estado seja null
+    e que ele faça uma transição ao sumir, sem desaparecer bruscamente
+    */
+    <AnimatePresence>
       {addedProduct && (
-        <div className="flex gap-4 items-center relative">
-          {/* Imagem do produto */}
-          <img
-            src={addedProduct.imageUrl}
-            alt="Produto adicionado ao carrinho"
-            className="w-2/6 bg-[#6A6868] rounded-sm"
-          />
-          {/* Informações do produto */}
-          <div className="flex flex-col gap-2">
-            <span className="text-[0.8rem] text-left">
-              {addedProduct.name} ({addedProduct.size})
-            </span>
-            <span className="text-[0.8rem] text-left">
-              {formPrice(addedProduct.price)}
-            </span>
-            {/* Mensagem de produto adicionado */}
-            <div className="flex justify-center items-center gap-2">
-              <Check className="size-4" />{" "}
-              <span className="text-[0.8rem]">Item adicionado ao carrinho</span>
+        /* O motion.div permite utilizar animações de um jeito mais prático
+          initial(estado que a animação começa): o container aparece 50px abaixo da posição inicial, com opacidade 0 e scale 0.9
+          animate(estado final da animação): o container aparece em sua posição original, com opacidade e scale 1
+          exit(estado de saída da animação): o container aparece 20px abaixo da posição original, com opacidade 0 e scale 0.95
+
+          Observação: com o frame-motion não é necessário utilizar o translate e o position para animações, apenas para centralização
+          */
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9, y: 50 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          exit={{ opacity: 0, scale: 0.95, y: 20, pointerEvents: "none" }}
+          transition={{ duration: 0.3 }}
+          className={`w-full max-w-sm z-50 
+          bg-[#1a1a1a] py-6 px-4 rounded-md border shadow border-white/10 text-white fixed left-1/2 -translate-y-1/2 -translate-x-1/2 top-1/2 sm:top-50 sm:left-auto sm:right-4 sm:translate-x-0 `}
+        >
+          <div className="flex gap-4 items-center relative">
+            {/* Imagem do produto */}
+            <img
+              src={addedProduct.imageUrl}
+              alt="Produto adicionado ao carrinho"
+              className="w-2/6 bg-[#6A6868] rounded-sm"
+            />
+            {/* Informações do produto */}
+            <div className="flex flex-col gap-2">
+              <span className="text-[0.8rem] text-left">
+                {addedProduct.name} ({addedProduct.size})
+              </span>
+              <span className="text-[0.8rem] text-left">
+                {formPrice(addedProduct.price)}
+              </span>
+              {/* Mensagem de produto adicionado */}
+              <div className="flex justify-center items-center gap-2">
+                <Check className="size-4" />{" "}
+                <span className="text-[0.8rem]">
+                  Item adicionado ao carrinho
+                </span>
+              </div>
+              {/* Botão que redireciona o usuário para a página do carrinho de compras */}
+              <button
+                type="button"
+                className={`text-center uppercase text-sm bg-black w-full px-6 py-2 rounded-sm hover:bg-[#0d0d0d]`}
+                onClick={handleCart}
+              >
+                Ver carrinho
+              </button>
             </div>
-            {/* Botão que redireciona o usuário para a página do carrinho de compras */}
-            <button
-              type="button"
-              className={`text-center uppercase text-sm bg-black w-full px-6 py-2 rounded-sm hover:bg-[#0d0d0d]`}
-              onClick={handleCart}
-            >
-              Ver carrinho
-            </button>
+            <X
+              className="absolute top-0 right-0 size-4 cursor-pointer"
+              onClick={handleClose}
+            />
           </div>
-          <X
-            className="absolute top-0 right-0 size-4 cursor-pointer"
-            onClick={handleClose}
-          />
-        </div>
+        </motion.div>
       )}
-    </div>
+    </AnimatePresence>
   );
 };
 
--- frontend/src/Components/cart/BuyDesktopMenu.tsx
+++ frontend/src/Components/cart/BuyDesktopMenu.tsx
@@ -6,6 +6,7 @@ import Size from "../products/Size";
 import { X } from "lucide-react";
 import { useEffect } from "react";
 import { useAddProduct } from "@/hooks/useAddProduct";
+import { motion, AnimatePresence } from "framer-motion";
 
 const BuyDesktopMenu = () => {
   const { selectedSize, setSelectedSize } = useSizeStore();
@@ -42,59 +43,69 @@ const BuyDesktopMenu = () => {
   }, [product]);
 
   return (
-    <div
-      className={`fixed inset-0 bg-black/60 z-40 transition-opacity duration-500 ${
-        product
-          ? "opacity-100 pointer-events-auto"
-          : "opacity-0 pointer-events-none"
-      }`}
-      onClick={closeMenu}
-    >
-      <div
-        className={`
+    /* O AnimatePresence é essencial na renderização condicional, em vez de usar a renderização no conteúdo do componente,
+    se usa em todo container, e o AnimatePresence com exit garantem que o componente permaneça na tela mesmo que o estado seja null
+    e que ele faça uma transição ao sumir, sem desaparecer bruscamente
+    */
+    <AnimatePresence>
+      {product && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={`fixed inset-0 bg-black/60 z-40 transition-opacity duration-500 `}
+          onClick={closeMenu}
+        >
+          {/* O motion.div permite utilizar animações de um jeito mais prático
+          initial(estado que a animação começa): o container aparece 30px abaixo da posição inicial, com opacidade 0 e scale 0.9
+          animate(estado final da animação): o container aparece em sua posição original, com opacidade e scale 1
+          exit(estado de saída da animação): o container aparece 20px abaixo da posição original, com opacidade 0 e scale 0.95
+
+          Observação: com o frame-motion não é necessário utilizar o translate e o position para animações, apenas para centralização
+          */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9, y: 30 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.95, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className={`
           fixed left-1/2 z-50 w-full max-w-md
           bg-[#1a1a1a] p-8 rounded-md border border-white/10 text-white
-          transition-all duration-700 ease-in-out
-          -translate-x-1/2 
-          ${
-            product
-              ? "top-1/2 -translate-y-1/2 opacity-100 scale-100"
-              : "top-full translate-y-0 opacity-0 scale-95"
-          }
+          -translate-x-1/2  translate-y-1/2
         `}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {product && (
-          <div className="flex flex-col items-start gap-4 relative">
-            {/* Nome do produto */}
-            {product && (
-              <span className="font-normal text-xl">{product.name}</span>
-            )}
-            {/* Preço do produto */}
-            {product && (
-              <span className="font-normal text-sm">
-                {formPrice(product.price)}
-              </span>
-            )}
-            {/* Componente dos tamanhos dos produtos */}
-            <Size className=" text-white border-white" />
-            {/* Botão com o evento de adicionar um item ao carrinho de compras */}
-            <button
-              type="submit"
-              className={`${selectedSize ? "disabled:opacity-100" : "disabled:opacity-50"} uppercase text-sm flex bg-black w-fit px-6 py-2 rounded-sm hover:bg-[#0d0d0d]`}
-              disabled={selectedSize ? false : true}
-              onClick={buyProduct}
-            >
-              comprar
-            </button>{" "}
-            <X
-              className="position absolute top-0 right-1 text-white"
-              onClick={closeMenu}
-            />
-          </div>
-        )}
-      </div>
-    </div>
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex flex-col items-start gap-4 relative">
+              {/* Nome do produto */}
+              {product && (
+                <span className="font-normal text-xl">{product.name}</span>
+              )}
+              {/* Preço do produto */}
+              {product && (
+                <span className="font-normal text-sm">
+                  {formPrice(product.price)}
+                </span>
+              )}
+              {/* Componente dos tamanhos dos produtos */}
+              <Size className=" text-white border-white" />
+              {/* Botão com o evento de adicionar um item ao carrinho de compras */}
+              <button
+                type="submit"
+                className={`${selectedSize ? "disabled:opacity-100" : "disabled:opacity-50"} uppercase text-sm flex bg-black w-fit px-6 py-2 rounded-sm hover:bg-[#0d0d0d]`}
+                disabled={selectedSize ? false : true}
+                onClick={buyProduct}
+              >
+                comprar
+              </button>{" "}
+              <X
+                className="position absolute top-0 right-1 text-white"
+                onClick={closeMenu}
+              />
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
